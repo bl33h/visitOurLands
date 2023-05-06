b82b48b657ed93f2d56be049d0a7410a31317460
Melissa Pérez <per21385@uvg.edu.gt>
2023-05-06 16:57:50 -0600

    profile frontend created

---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import './Profile.css'
+import '/src/Components/texts.css'
+import '/src/Components/display.css'
+
+function Profile(){
+
+    return(
+        <div className="root">
+            <div className="container">
+                <div className="info">
+                    <img id="profile-picture" src="/src/assets/profile.png" />
+                    <div className="column">
+                        <h1 className="username">Username</h1>
+                        <p className="description">Description</p>
+                    </div>
+                </div>
+
+                <div id="edit" className="buttons">
+                    <button className="each-button" 
+                        style={{
+                            backgroundImage: "url('/src/assets/1.png')",
+                            backgroundSize: "cover",
+                            backgroundPosition: "center"
+                        }}
+                        onClick={() => {
+                            //colocar la accion
+                        }}>
+                    </button>
+
+                    <button id="save" className="each-button" 
+                        style={{
+                            backgroundImage: "url('/src/assets/2.png')",
+                            backgroundSize: "cover",
+                            backgroundPosition: "center"
+                        }}
+                        onClick={() => {
+                            //colocar la accion
+                        }}>
+                    </button>
+
+                    <button id="like" className="each-button" 
+                        style={{
+                            backgroundImage: "url('/src/assets/3.png')",
+                            backgroundSize: "cover",
+                            backgroundPosition: "center"
+                        }}
+                        onClick={() => {
+                            //colocar la accion
+                        }}>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
