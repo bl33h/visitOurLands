23783947c0f12eb555efcb281d56ed58aff4f877
Melissa Pérez <per21385@uvg.edu.gt>
2023-09-03 23:31:48 -0600

    saves comment correctly

--- src/Pages/TopRec/interactions/comment/comment.jsx
+++ src/Pages/TopRec/interactions/comment/comment.jsx
@@ -1,70 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '/src/Components/texts.css';
 import '/src/Components/display.css';
 import { supabase } from '../../../../client';
-import 'comment.css'
+import './comment.css';
 
-function Comment(){
+function Comment({ selectedPlaceId }) {
+  const [placeInfo, setPlaceInfo] = useState({});
+  const [comment, setComment] = useState('');
+  const [user, setUser] = useState(null);
 
-    const [placeInfo, setPlaceInfo] = useState(null);
-    const [comment, setComment] = useState('');
-
-    useEffect(() => {
-        async function fetchPlaceInfo() {
-          if (selectedPlaceId) {
-            const { data, error } = await supabase
-              .from('places')
-              .select('name, description, image')
-              .eq('id', selectedPlaceId)
-              .single();
-    
-            if (error) {
-              console.error('Error fetching place info:', error);
-            } else {
-              setPlaceInfo(data);
-            }
-          }
-        }
-
-        fetchPlaceInfo();
-    }, [selectedPlaceId]);
-
-    if (!placeInfo) {
-        return <div>Loading...</div>;        
-    }
-
-    async function saveComment() {
-        if (comment.trim() === '') return;
-    
+  useEffect(() => {
+    async function fetchPlaceInfo() {
+      if (selectedPlaceId) {
         const { data, error } = await supabase
-          .from('comments')
-          .insert([{ place_id: selectedPlaceId, comment }])
+          .from('places')
+          .select('name, description, image')
+          .eq('id_places', selectedPlaceId)
           .single();
-    
+
         if (error) {
-          console.error('Error saving comment:', error);
+          console.error('Error fetching place info:', error);
         } else {
-          console.log('Comment saved:', data);
-          setComment('');
+          setPlaceInfo(data);
         }
+      }
     }
-    
-    return(
-        <div className="root">
-            <div className="container">
-                <h2>Sección de comentarios</h2>
-                <h3>{placeInfo.name}</h3>
-                <p>{placeInfo.description}</p>
-                <img src={recommendation.image} alt={recommendation.name} />
-                <textarea
-                    value={comment}
-                    onChange={(e) => setComment(e.target.value)}
-                    placeholder="Escribe tu comentario..."
-                />
-                <button onClick={saveComment}>Guardar comentario</button>
-            </div>
-        </div>
-    );
+
+    fetchPlaceInfo();
+  }, [selectedPlaceId]);
+
+  useEffect(() => {
+    // Obtener el usuario del localStorage
+    const userJSON = window.localStorage.getItem('LOGIN_STATUS');
+    if (userJSON) {
+      const user = JSON.parse(userJSON);
+      setUser(user);
+    }
+  }, []);
+
+  async function saveComment() {
+    if (comment.trim() === '') return;
+
+    if (!user || !user.username) {
+      console.error('Usuario no válido o sin nombre.');
+      return;
+    }
+
+    if (!selectedPlaceId) {
+      console.error('ID de lugar no encontrado.');
+      return;
+    }
+
+    try {
+      const { data, error } = await supabase
+        .from('comments')
+        .insert([
+          {
+            id_places: selectedPlaceId,
+            username: user.username,
+            message: comment.trim(),
+          },
+        ])
+        .single();
+
+        if (error) {
+          console.error('Error al guardar el comentario:', error);
+        } else {
+        console.log('Comentario guardado:', data);
+        setComment('');
+      }
+    } catch (error) {
+        console.error('Error al guardar el comentario:', error);
+    }
+  }
+
+  return (
+    <div className="root">
+      <div className="container">
+        <h2>Sección de comentarios</h2>
+        <textarea
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          placeholder="Escribe tu comentario..."
+        />
+        <button onClick={saveComment}>Guardar comentario</button>
+      </div>
+    </div>
+  );
 }
 
 export default Comment;
