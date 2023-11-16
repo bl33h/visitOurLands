e7917d68ddd72c9a33574f02ee7a88188d1718be
SebasJuarez <juaressebastian@gmail.com>
2023-11-15 23:39:03 -0600

    Share link message updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Page from './Pages'
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
     <div className="App">
+      <ToastContainer />
       <BrowserRouter>
         <Page />
       </BrowserRouter>
