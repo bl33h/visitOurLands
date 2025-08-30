bde58042f849d1c78e1669b6a50a2df08be1926f
SebasJuarez <juaressebastian@gmail.com>
2025-08-30 11:39:49 -0600

    Database changes

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-    "https://cchcbbjgpcwuugrjedsl.supabase.co/", 
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjaGNiYmpncGN3dXVncmplZHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMzMjk2NDEsImV4cCI6MTk5ODkwNTY0MX0.AehLrTlHHIt04_0Xw4B6iEO3EKXpG9H5EG2NiKCZtAw",
+    "https://nfxfnlnnuzpeflfaqxzc.supabase.co/", 
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5meGZubG5udXpwZWZsZmFxeHpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1NzIyOTcsImV4cCI6MjA3MjE0ODI5N30.CbjcBuB05FKYctTtnCZvsFxPDVtGDFHAg29aiqOQNoQ",
 )
